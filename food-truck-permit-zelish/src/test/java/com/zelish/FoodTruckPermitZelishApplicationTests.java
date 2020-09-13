759be01ce2687640976462b0809dfe48bac23984
Akshay Havale <akshayhavale22@gmail.com>
2020-09-13 11:47:28 +0530

    Project-Setup

---
+++ food-truck-permit-zelish/src/test/java/com/zelish/FoodTruckPermitZelishApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zelish;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodTruckPermitZelishApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
