114b320ecb9bbeca74e125dbad370fcf2e675509
Akshay Havale <akshayhavale22@gmail.com>
2020-09-13 16:27:29 +0530

    changes

--- food-truck-permit-zelish/src/main/java/com/zelish/services/foodtruck/FoodTruckServiceImpl.java
+++ food-truck-permit-zelish/src/main/java/com/zelish/services/foodtruck/FoodTruckServiceImpl.java
@@ -1,5 +1,61 @@
 package com.zelish.services.foodtruck;
 
-public class FoodTruckServiceImpl {
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.zelish.model.FoodTruck;
+
+@Service
+public class FoodTruckServiceImpl implements FoodTruckService{
+	
+
+	@Override
+	public FoodTruck create(FoodTruck foodTruck) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public FoodTruck update(FoodTruck foodTruck, long locationid) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public FoodTruck getOne(long locationid) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<FoodTruck> getAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void delete(long locationid) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<FoodTruck> getByApplicant(String applicant) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<FoodTruck> getByFacilityType(String facilityType) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<FoodTruck> getByStatus(String status) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
